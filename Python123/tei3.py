3acfc9491d265ed6d6b2e7b8e9559db8b354eb7e
Arthurflsimao <arthur.simao@sptech.school>
2022-11-29 14:39:00 -0300

    Python esta funcionando e dashboard em desenvolvimento

--- Python123/tei3.py
+++ Python123/tei3.py
@@ -21,18 +21,18 @@ from mysql.connector import errorcode
 #         print("="*40, "System Information", "="*40)
 # uname = platform.uname()
 
-def ConectarBancoLocal():
-    try:
-        conn = mysql.connector.connect(
+
+try:
+        db_connection = mysql.connector.connect(
             host='localhost',
             user='aluno',
             password='sptech',
             database='fronttier2'
         )
         print("Conexão com o Banco de Dados MySQL efetuada com sucesso!")
-        LeituraLocal(conn)
+        # LeituraLocal(conn)
     # Validações de Erro:
-    except mysql.connector.Error as err:
+except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("Algo está errado com o Usuário do Banco ou a Senha.")
             time.sleep(10)
@@ -44,20 +44,29 @@ def ConectarBancoLocal():
             time.sleep(10)
 
 # Inserir leituras Banco local
-def LeituraLocal(conn):
-    discoTotal = round((psutil.disk_usage('/')[0] / 10**12), 3)
-    discoUsado = round((psutil.disk_usage('/')[1] / 10**12), 3)
-    discoLivre = round((psutil.disk_usage('/')[2] / 10**12), 3)
+# def LeituraLocal(conn):
+while True:
+    
+    discoTotal = round((psutil.disk_usage('/')[0] / 10**12), 2)
+    discoUsado = round((psutil.disk_usage('/')[1] / 10**12), 2)
+    discoLivre = round((psutil.disk_usage('/')[2] / 10**12), 2)
     porcentagem = psutil.disk_usage('/')[3]
     discoLido = round(psutil.net_io_counters()[1] / 10**6, 2)
     discoEscrito = round(psutil.net_io_counters()[0] / 10**6, 2)
 
-    datahora = datetime.now().strftime('%Y/%m/%d %H:%M:%S')
+    datahora = datetime.now()
+    # formato = datahora.strftime("%d/%m/%Y %H:%M:%S")
 
-    crsr = conn.cursor()
+    crsr = db_connection.cursor()
     
-    crsr.execute("INSERT INTO Dados (dataHora, discoTotal, discoUso, discoLivre, porcentagem, discoLido, discoEscrito) VALUES (%s,%s,%s,%s,%s,%s,%s)"(datahora, discoTotal, discoUsado, discoLivre, porcentagem, discoLido, discoEscrito))
-    crsr.commit()
+    sql = "INSERT INTO Disco (dataHora, discoTotal, discoUso, discoLivre, porcentagem, discoLido, discoEscrito) VALUES (%s,%s,%s,%s,%s,%s,%s)"
+    values = [datahora, discoTotal, discoUsado, discoLivre, porcentagem, discoLido, discoEscrito]
+    crsr.execute(sql, values)
+
     print("Inserindo leitura no banco de dados local!")
     
-ConectarBancoLocal()
+    db_connection.commit()
+
+    time.sleep(3.0)
+    
+# ConectarBancoLocal()
